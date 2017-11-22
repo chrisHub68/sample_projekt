451d40ecdc9116e2af66ebe5f73fab65d9127dbd
C.Huber <chris.41@hotmail.de>
2017-11-22 18:34:21 +0100

    added bug

--- src/main/java/com/javacodegeeks/examples/junitmavenexample/Calculator.java
+++ src/main/java/com/javacodegeeks/examples/junitmavenexample/Calculator.java
@@ -14,12 +14,12 @@ public class Calculator implements ICalculator {
 
 	@Override
 	public int multiplication(int a, int b) {
-		return a + b;
+		return a * b;
 	}
 
 	@Override
 	public int divison(int a, int b) throws Exception {
-		if (b == 0) {
+		if (b == _1) {
 			throw new Exception("Divider can't be zero");
 		}
 
